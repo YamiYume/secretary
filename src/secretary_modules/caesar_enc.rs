b058e611eb1d9f049ab10181f9e597d7546706f5
YamiYume <p.steven.t.g@gmail.com>
2022-08-22 00:26:00 -0500

    first widget functional

--- src/secretary_modules/caesar_enc.rs
+++ src/secretary_modules/caesar_enc.rs
@@ -43,35 +43,52 @@ impl View for CaesarEnc {
         let plaintext_edit = egui::TextEdit::multiline(plaintext)
             .hint_text("Write your plaintext here")
             .show(ui);
+        let popup_id = ui.make_persistent_id("Error_popup");
         let key_selector = ui.add(egui::Slider::new(key, 0..=26));
-        ui.add_enabled(false, egui::TextEdit::multiline(ciphertext));
+        ui.horizontal_top(|ui| {
+            ui.add_enabled(false, egui::TextEdit::multiline(ciphertext));
+            ui.vertical_centered_justified(|ui| {
+                if ui.add(egui::Button::new("Copy")).clicked() {
+                    ui.output().copied_text = ciphertext.to_string();
+                }
+                if ui.add(egui::Button::new("Copy Key")).clicked() {
+                    ui.output().copied_text = key.to_string();
+                }
+            });
+        });
         if plaintext_edit.response.changed() | key_selector.changed() {
             if self.valid_plaintext() {
+                ui.memory().close_popup();
                 self.update_ciphertext()
+            } else {
+                ui.memory().open_popup(popup_id);
             }
         }
+        egui::popup::popup_below_widget(ui, popup_id, &plaintext_edit.response, |ui| {
+            ui.code("Unvalid plaintext, must be lowercase alphabetic");
+        });
     }
 }
 
 impl CaesarEnc {
     fn update_ciphertext(&mut self) -> () {
-        let plaintext_whiteless: String = self.plaintext
+        let plaintext_whiteless: String = self
+            .plaintext
             .chars()
             .filter(|c| !c.is_ascii_whitespace())
             .collect();
         let mut ciphertext_build = String::from("");
         for c in plaintext_whiteless.chars() {
-            let position = (c as u32 + self.key - 61) % 26 + 40;
+            let position = (c as u32 + self.key - 97) % 26 + 65;
             ciphertext_build.push(char::from_u32(position).unwrap());
         }
         self.ciphertext = ciphertext_build;
     }
     fn valid_plaintext(&self) -> bool {
-        let mut is_valid: bool = true;
-        is_valid &= self.plaintext.is_ascii();
-        is_valid &= self.plaintext
+        let is_valid = self
+            .plaintext
             .chars()
-            .all(|c| c.is_alphabetic());
+            .all(|c| c.is_ascii_lowercase() | c.is_ascii_whitespace());
         is_valid
     }
 }
